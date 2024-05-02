4c1b29944ce5da4e20e179ce56d4b3eb29e52e9d
patricionarbona <patricioinarbona@gmail.com>
2024-05-02 10:07:42 +0200

    v1-carta pelicula

--- buscador-peliculas/src/components/pelicula/Pelicula.jsx
+++ buscador-peliculas/src/components/pelicula/Pelicula.jsx
@@ -6,10 +6,8 @@ function Pelicula({rutaFoto, titulo, year}) {
     return (
         <>
             <div className="relative z-10 h-56 md:h-48 w-40">
-                <img className="h-52 md:h-48" src={rutaFoto}
-
-    alt="" />
-                <div className="absolute z-10 -bottom-5 md:-bottom-10">
+                <img className="h-52 w-36 md:h-48" src={rutaFoto} alt="" />
+                <div className="absolute z-10 -bottom-5 md:-bottom-10 bg-slate-500 bg-opacity-10 w-36 ">
                     <h2 className="uppercase">{titulo}</h2>
                     <p>{year}</p>
                 </div>
